46aa1c242aeaaf7f8d1889a0177cb5ae3575bb21
sverrejoh <sverre.johansen@gmail.com>
2010-03-02 13:13:12 +0100

    Fixed the test command to be compatible with South tip

--- django_nose/management/commands/test.py
+++ django_nose/management/commands/test.py
@@ -3,15 +3,13 @@ Add extra options from the test runner to the ``test`` command, so that you can
 browse all the nose options from the command line.
 """
 from django.conf import settings
-from django.core.management.commands.test import Command
 from django.test.utils import get_runner
 
 
 try:
-    """I wish south worked with Django trunk."""
-    # from south.management.commands.test import Command
+    from south.management.commands.test import Command
 except ImportError:
-    pass
+    from django.core.management.commands.test import Command
 
 
 TestRunner = get_runner(settings)
