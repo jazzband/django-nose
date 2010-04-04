95224da260aa1870ea648f660dcfba2d4f6bad0c
Jeff Balogh <me@jeffbalogh.org>
2010-04-04 15:22:57 -0700

    dirty tests are better than no tests

---
+++ testapp/settings_with_south.py
@@ -0,0 +1,3 @@
+from settings import *
+
+INSTALLED_APPS = ('south',) + INSTALLED_APPS
---
+++ testapp/test_for_nose.py
@@ -0,0 +1,7 @@
+"""
+Django's test runner won't find this, but nose will.
+"""
+
+
+def test_addition():
+    assert 1 + 1 == 2
