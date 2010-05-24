48d545249a61670a72e1de3aa998bd036cc80054
Markus Törnqvist <mjt@fadconsulting.com>
2010-05-24 21:06:38 +0300

    Content in Pythonesque functions

--- django_nose/assertions.py
+++ django_nose/assertions.py
@@ -7,27 +7,36 @@ Assertions that sort of follow Python unittest/Django test cases
 ## Python
 
 def fail_unless_equal(first, second, msg=None):
-    assert False
+    assert first == second, (msg or '%s != %s' % (first, second))
 
 assert_equal = assert_equals = fail_unless_equal
 
 def fail_if_equal(first, second, msg=None):
-    assert False
+    assert first != second, (msg or '%s == %s' % (first, second))
 
 assert_not_equal = assert_not_equals = fail_if_equal
 
 def fail_unless_almost_equal(first, second, places=7, msg=None):
-    assert False
+    assert round(abs(second-first), places) != 0, (msg or '%s == %s within %s places' % (first, second, places))
 
 assert_almost_equal = assert_almost_equals = fail_unless_almost_equal
 
 def fail_if_almost_equal(first, second, places=7, msg=None):
-    assert False
+    assert round(abs(second-first), places) == 0, (msg or '%s != %s within %s places' % (first, second, places))
 
 assert_not_almost_equal = assert_not_almost_equals = fail_if_almost_equal
 
 def fail_unless_raises(exc_class, callable_obj, *args, **kwargs):
-    assert False
+    try:
+        callable_obj(*args, **kwargs)
+    except exc_class:
+        return
+    else:
+        if hasattr(exc_class, '__name__'):
+            exc_name = exc_class.__name__
+        else:
+            exc_name = str(exc_class)
+        raise AssertionError('%s not raised' % exc_name)
 
 assert_raises = fail_unless_raises
 
