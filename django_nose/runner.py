ae9dfee2389b278e40f39a741fd7edcaa0a68470
Martin Chase <outofculture@gmail.com>
2010-08-04 15:09:32 -0700

    include arg-parser options that are in OptionGroups (does not also do proper grouping of those options for printing purposes).

--- django_nose/runner.py
+++ django_nose/runner.py
@@ -92,7 +92,7 @@ def _get_options():
     cfg_files = nose.core.all_config_files()
     manager = nose.core.DefaultPluginManager()
     config = nose.core.Config(env=os.environ, files=cfg_files, plugins=manager)
-    options = config.getParser().option_list
+    options = config.getParser()._get_all_options()
     django_opts = [opt.dest for opt in BaseCommand.option_list] + ['version']
     return tuple(o for o in options if o.dest not in django_opts and
                                        o.action != 'help')
