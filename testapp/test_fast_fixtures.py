522841a8cc6c4a602e490a2743e6cbf5d6a57b71
Ben Lopatin <ben@benlopatin.com>
2016-03-11 12:31:46 -0500

    Add test case for the FastFixturesTestCase class
    Test fails against Django 1.8+

---
+++ testapp/test_fast_fixtures.py
@@ -0,0 +1,27 @@
+"""
+Tests for FastFixtureTestCase.
+
+These tests primarily verify that the TestCases load and tests run rather than
+specifically verifying how the TestCase handles the fixtures themselves.
+"""
+
+from datetime import datetime
+
+from django_nose.testcases import FastFixtureTestCase
+from testapp.models import Question
+
+
+class HasFixturesTestCase(FastFixtureTestCase):
+    """Tests that use a test fixture."""
+
+    fixtures = ["testdata.json"]
+
+    def test_fixture_loaded(self):
+        """Test that a FAST fixture was loaded."""
+        question = Question.objects.get()
+        self.assertEqual(
+            'What is your favorite color?', question.question_text)
+        self.assertEqual(datetime(1975, 4, 9), question.pub_date)
+        choice = question.choice_set.get()
+        self.assertEqual("Blue.", choice.choice_text)
+        self.assertEqual(3, choice.votes)
